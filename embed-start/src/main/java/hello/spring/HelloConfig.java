2a7a4ceecd1a9562d1b408d4ad7ea3ea9dac8705
yhyoon1004 <yhyoon1004@naver.com>
2024-04-02 20:05:52 +0900

    편리한 스프링부트 클래스 만들기

--- embed-start/src/main/java/hello/spring/HelloConfig.java
+++ embed-start/src/main/java/hello/spring/HelloConfig.java
@@ -3,9 +3,8 @@ package hello.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class HelloConfig {
-
     @Bean
     public HelloController helloController() {
         return new HelloController();
--- embed-start/src/main/java/hello/spring/HelloController.java
+++ embed-start/src/main/java/hello/spring/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/hello-spring")
     public String hello() {
         System.out.println("HelloController.hello");

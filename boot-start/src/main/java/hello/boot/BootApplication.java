96cf5fb91b8d480806988d5f87323906bf2fd272
yhyoon1004 <yhyoon1004@naver.com>
2024-04-03 18:55:43 +0900

    스프링 부트와 웹서버 - 실행 과정

--- boot-start/src/main/java/hello/boot/BootApplication.java
+++ boot-start/src/main/java/hello/boot/BootApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootApplication {
 
 	public static void main(String[] args) {
+		//본 클래스를 넘겨준 이유는 @SpringBootApplication 어노테이션의 기능 때문에
+		//@SpringBootApplication 안에는 컴포넌트 스캔을 포함한 여러기능을 포함한 여러 기능이 설정되어 있음
 		SpringApplication.run(BootApplication.class, args);
+		// 핵심은 2가지 1. 스프링컨테이너 생성  2. WAS 내장 톰캣 생성
 	}
 
 }

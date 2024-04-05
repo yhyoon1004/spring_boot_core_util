3bfd6aebbaa0f5ffcad44a0091f0d479ffd5a932
yhyoon1004 <yhyoon1004@naver.com>
2024-04-05 20:47:18 +0900

    @Conditional

--- autoconfig-start/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig-start/src/main/java/hello/config/MemoryConfig.java
@@ -1,12 +1,15 @@
 package hello.config;
 
 import memory.Memory;
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Conditional(MemoryCondition.class) //이 어노테이션을 사용하여 미리 만들어준 조건 클래스를 넣어 조건에 맞게 초기화 되도록 설정할 수 있다.
 public class MemoryConfig {
     @Bean
     public MemoryController memoryController() {
---
+++ autoconfig-start/src/main/java/memory/MemoryCondition.java
@@ -0,0 +1,17 @@
+package memory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+@Slf4j
+public class MemoryCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        // -Dmemory=on  java 환경 정보에 memory=on 일 경우에 호출 되도록 설정
+        String memory = context.getEnvironment().getProperty("memory");
+        log.info("memor y = {}", memory);
+        return "on".equals(memory);
+    }
+}
